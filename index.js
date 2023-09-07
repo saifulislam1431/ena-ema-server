1a38d5b7f36357b544f4b4cc4f6360f65f50ee81
Saiful Islam <sakibbhaiya143@gmail.com>
2023-09-07 10:35:21 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+app.get("/", (req,res)=>{
+    res.send("Server Is Running")
+})
+
+app.listen(port , ()=>{
+    console.log(`This server listening at port ${port}`);
+})
