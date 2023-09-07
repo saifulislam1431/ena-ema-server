2077a46ba97dae543ae75a9dbccdc7ef6ef77b66
Saiful Islam <sakibbhaiya143@gmail.com>
2023-09-08 02:08:55 +0600

    profile API update

--- index.js
+++ index.js
@@ -151,8 +151,9 @@ async function run() {
           res.send(result)
         })
 
-        app.get("/profile/:email", verifyJWT, verifyAdmin, async (req, res) => {
+        app.get("/profile", verifyJWT, verifyAdmin, async (req, res) => {
           const email = req.query.email;
+          console.log(email);
           const query = {email: email}
           if(query){
             const result = await userCollection.findOne({query});
